e18980e67ae990f30496c9dd90b32a1fc698107b
GoldenMan <67754398+Hardeybaryor@users.noreply.github.com>
2025-06-25 21:26:02 +0200

    Add initial aiclass.py with commented code examples
    Introduces aiclass.py, containing commented-out Python print statements and explanations for basic concepts such as formatted strings, number calculations, and multiline strings. This serves as a template or instructional reference for beginners.

---
+++ aiclass.py
@@ -0,0 +1,27 @@
+# print("Hello World!")
+# print ("Hello, Andrew")
+# print ("Hello, Adeabyo")
+# print ("White and Blue are my favourite colours")
+
+# to answer the question how are you feeling today?
+# print ("Well I'm fine, just a bit overwehlmed with alot to learn")
+
+# alter the code to print an error message
+# print ("Well I'm fine, just a bit overwehlmed with alot to learn)
+
+# Triple Quote String = Multiline
+# print("""Hello Mr Victor, This AI course is really great and well explained""")
+
+# Number calculations and co
+# print(10+20+30+40+50)
+
+# Format String
+# string = Text
+# float = decimal
+# integars = numbers
+
+# Formatted Strings looks for things within {}
+# print("The Temp 75F in degree Celcius is ((75-32)* 5/9)C") --- This would be wrong because it's not sending a direct message
+
+# The correct code line is below
+# print(f"The Temp 75F in degree Celcius is {(75-32)* 5/9}C") --- This would be correct because it's sending a direct message)
