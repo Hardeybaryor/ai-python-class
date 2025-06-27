23a32e6e753927dc08b34e87af1498a40b78acd7
GoldenMan <67754398+Hardeybaryor@users.noreply.github.com>
2025-06-27 22:27:47 +0200

    Update aiclass.py

--- aiclass.py
+++ aiclass.py
@@ -27,4 +27,30 @@
 # print (f"The Temp 75F in degree Celcius is {(75-32)* 5/9}C") --- This would be correct because it's sending a direct message)
 
 # print (f"My tenant's dog is {28/4} years old")
-# Note this output will be including
+# Note this output will be include decimals like 4.0/7.0
+
+# print(f"My tenant's dog is {28/4:.0f} years old")
+# Note this output will be not include decimals like 4.0/7.0
+# :.2f (2 dp)
+# :.3f (3 dp)
+
+
+#   Variables
+# variables can be used to store string or numbers
+
+# age = 28
+# print(age)
+
+# name = "Adebayo"
+# height = 5.7
+# print(f"Age: {age}")
+
+# print(f"Name: {name}")
+
+
+# __________Using llm on Python____________
+# from helper_functions import print_llm_response
+# fav_movie = "Matrix"
+# fav_food = "Amala"
+# fav_game = "Ludo"
+# print_llm_response (f"recommend to me a new song to listen to based on my likes of {fav_movie}, {fav_food} and {fav_game}?")
